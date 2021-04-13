8848cdf477167088d88d641f599870467c27d817
Dmitri611 <dimakkk1181@mail.ru>
2021-04-13 19:19:30 +0300

    feat: complited 11 15

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,31 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = [];
+  const arr1 = [];
+  let count = 1;
+  let curr;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      curr = str[i];
+      count++;
+    }
+    if (count === 1) {
+      curr = str[i];
+    }
+    if (str[i] !== str[i + 1]) {
+      arr.push(count, curr);
+      count = 1;
+      curr = str[i + 1];
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 1) {
+      arr1.push(arr[i]);
+    }
+  }
+  return arr1.join('').toString();
 }
 
 module.exports = encodeLine;
